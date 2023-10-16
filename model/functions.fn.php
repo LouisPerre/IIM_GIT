7f9b9aea1bf9d1fc52ad3f332ff3d70122f67832
Adel Khiter <khiter.adel@gmail.com>
2023-10-16 12:44:28 +0200

    fix(userRegistration): Add default value for picture field

--- model/functions.fn.php
+++ model/functions.fn.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 /*******************************************************************
 SUMMARY
 	1!FUNCTIONS
@@ -14,14 +15,14 @@ SUMMARY
 		2.0!isMusicOwner
 		2.1!updateProfilPicture	
 
-********************************************************************/
+ ********************************************************************/
 
 /**************************************************
 					1!FUNCTIONS
-**************************************************/
-		
+ **************************************************/
 
-	/*1.1!userRegistration
+
+/*1.1!userRegistration
 		return :
 			true for registration OK
 			false for fail
@@ -30,26 +31,27 @@ SUMMARY
 		$email -> 			field value : email
 		$password -> 		field value : password
 	*/
-	function userRegistration(PDO $db, $username, $email, $password){
-		$sql = "INSERT INTO users
+function userRegistration(PDO $db, $username, $email, $password)
+{
+	$sql = "INSERT INTO users
 				  SET
 				  username = :username,
 				  email = :email,
-				  password = :password";
+				  password = :password,
+				  picture = :picture";
 
 
-		$req = $db->prepare($sql);
-		$req->execute(array(
-			':username' => $username,
-			':email' => $email,
-			':password' => $password,
-		));
-		
+	$req = $db->prepare($sql);
+	$req->execute(array(
+		':username' => $username,
+		':email' => $email,
+		':password' => $password,
+		':picture' => 'lala'
+	));
+}
 
-	}
 
-
-	/*1.2!userConnection
+/*1.2!userConnection
 		return :
 			true for connection OK
 			false for fail
@@ -57,74 +59,77 @@ SUMMARY
 		$email -> 			field value : email
 		$password -> 		field value : password
 	*/
-	function userConnection(PDO $db, $email, $password){
-		if(!empty($email) && !empty($password)){
-			//Requête SQL
-			$sql = "SELECT * FROM users WHERE email = :email AND password = :password LIMIT 1";
+function userConnection(PDO $db, $email, $password)
+{
+	if (!empty($email) && !empty($password)) {
+		//Requête SQL
+		$sql = "SELECT * FROM users WHERE email = :email AND password = :password LIMIT 1";
 
-			$req = $db->prepare($sql);
-			$req->execute(array(
-				':email' => $email,
-				':password' => $password
-			));
+		$req = $db->prepare($sql);
+		$req->execute(array(
+			':email' => $email,
+			':password' => $password
+		));
 
-			$result = $req->fetch(PDO::FETCH_ASSOC);
-			//Si le fetch réussi, alors un résultat a été retourné donc le couple email / password est correct
-			if($result == true){
-				
-				//on définit la SESSION
-				$_SESSION['id'] = $result['id'];
-				$_SESSION['username'] = $result['username'];
-				$_SESSION['email'] = $result['email'];
-				$_SESSION['created_at'] = $result['created_at'];
-				$_SESSION['image'] = $result['picture'];
+		$result = $req->fetch(PDO::FETCH_ASSOC);
+		//Si le fetch réussi, alors un résultat a été retourné donc le couple email / password est correct
+		if ($result == true) {
 
-				return true;
-			}else{
-				return false;
-			}
-		}else{
+			//on définit la SESSION
+			$_SESSION['id'] = $result['id'];
+			$_SESSION['username'] = $result['username'];
+			$_SESSION['email'] = $result['email'];
+			$_SESSION['created_at'] = $result['created_at'];
+			$_SESSION['image'] = $result['picture'];
 
+			return true;
+		} else {
 			return false;
 		}
-	}
+	} else {
 
-	/*1.3!listMusics
+		return false;
+	}
+}
+
+/*1.3!listMusics
 		return :
 			array of results
 		$db -> 				database object
 	*/
-	function listMusics(PDO $db){
-		$sql = "SELECT musics.*, users.id AS user_id, users.username, users.picture FROM musics LEFT JOIN users ON musics.user_id = users.id ORDER BY musics.created_at DESC";
+function listMusics(PDO $db)
+{
+	$sql = "SELECT musics.*, users.id AS user_id, users.username, users.picture FROM musics LEFT JOIN users ON musics.user_id = users.id ORDER BY musics.created_at DESC";
 
-		$req = $db->prepare($sql);
-		$req->execute();
+	$req = $db->prepare($sql);
+	$req->execute();
 
-		$results = $req->fetchAll(PDO::FETCH_ASSOC);
+	$results = $req->fetchAll(PDO::FETCH_ASSOC);
 
-		return $results;
-	}
+	return $results;
+}
 
-	/*1.4!selectMusic
+/*1.4!selectMusic
 		return :
 			array
 		$db -> 				database object
 		$music_id -> 		value : music_id
 	*/
-	function selectMusic(PDO $db, $music_id){
-		$sql = "SELECT * FROM musics WHERE id = :id";
+function selectMusic(PDO $db, $music_id)
+{
+	$sql = "SELECT * FROM musics WHERE id = :id";
 
-		$req = $db->prepare($sql);
-		$req->execute(array(
-			':id' => $music_id
-		));
+	$req = $db->prepare($sql);
+	$req->execute(array(
+		':id' => $music_id
+	));
 
-		$result = $req->fetch(PDO::FETCH_ASSOC);
+	$result = $req->fetch(PDO::FETCH_ASSOC);
 
-		return $result;
-	}
+	return $result;
+}
 
-	/*1.5!addMusic
+/*1.5!addMusic
 		return :
 			true if music added
 		$db -> 			database object
@@ -132,8 +137,9 @@ SUMMARY
 		$title -> 		field value : title
 		$file -> 		file object : music
 	*/
-	function addMusic(PDO $db, $user_id, $title, $file){
-		$sql = "
+function addMusic(PDO $db, $user_id, $title, $file)
+{
+	$sql = "
 			INSERT INTO
 				musics
 			SET
@@ -142,17 +148,17 @@ SUMMARY
 				file = :file
 		";
 
-		$req = $db->prepare($sql);
-		$req->execute(array(
-			':user_id' => $user_id,
-			':title' => $title,
-			':file' => $file,
-		));
+	$req = $db->prepare($sql);
+	$req->execute(array(
+		':user_id' => $user_id,
+		':title' => $title,
+		':file' => $file,
+	));
 
-		return true;
-	}
+	return true;
+}
 
-	/*1.6!updateMusic
+/*1.6!updateMusic
 		return :
 			true if music updated
 			false if not updated
@@ -161,11 +167,12 @@ SUMMARY
 		$title -> 		field value : title
 		$user_id -> 	current user id
 	*/
-	function updateMusic(PDO $db, $music_id, $title, $user_id){
-		$music_owner = isMusicOwner($db, $music_id, $user_id);
+function updateMusic(PDO $db, $music_id, $title, $user_id)
+{
+	$music_owner = isMusicOwner($db, $music_id, $user_id);
 
-		if($music_owner == true){
-			$sql = "
+	if ($music_owner == true) {
+		$sql = "
 				UPDATE
 					musics
 				SET
@@ -174,20 +181,19 @@ SUMMARY
 					 id = :id
 			";
 
-			$req = $db->prepare($sql);
-			$req->execute(array(
-				':title' => $title,
-				':id' => $music_id
-			));
+		$req = $db->prepare($sql);
+		$req->execute(array(
+			':title' => $title,
+			':id' => $music_id
+		));
 
-			return true;
-		}
-		else{
-			return false;
-		}
+		return true;
+	} else {
+		return false;
 	}
+}
 
-	/*1.7!deleteMusic
+/*1.7!deleteMusic
 		return :
 			true if music deleted
 			false if not deleted
@@ -196,51 +202,54 @@ SUMMARY
 		$title -> 		field value : title
 		$music_id -> 	music id
 	*/
-	function deleteMusic(PDO $db, $music_id, $user_id){
-		$music_owner = isMusicOwner($db, $music_id, $user_id);
+function deleteMusic(PDO $db, $music_id, $user_id)
+{
+	$music_owner = isMusicOwner($db, $music_id, $user_id);
 
-		if($music_owner == true){
-			$sql = "DELETE FROM musics WHERE id = :id";
-
-			$req = $db->prepare($sql);
-			$req->execute(array(':id' => $music_id));
-
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-
-	/*1.8!isEmailAvailable*/
-	function isEmailAvailable(PDO $db, $email){
-		$sql = "SELECT id FROM users WHERE email = :email LIMIT 1";
+	if ($music_owner == true) {
+		$sql = "DELETE FROM musics WHERE id = :id";
 
 		$req = $db->prepare($sql);
-		$req->execute(array(':email' => $email));
+		$req->execute(array(':id' => $music_id));
 
-		$result = $req->fetch(PDO::FETCH_ASSOC);
-
-		if(!empty($result)) return false;
-		else return true;
+		return true;
+	} else {
+		return false;
 	}
+}
 
-	/*1.9!isUsernameAvailable*/
-	function isUsernameAvailable(PDO $db, $username){
-		$sql = "SELECT id FROM users WHERE username = :username LIMIT 1";
+/*1.8!isEmailAvailable*/
+function isEmailAvailable(PDO $db, $email)
+{
+	$sql = "SELECT id FROM users WHERE email = :email LIMIT 1";
 
-		$req = $db->prepare($sql);
-		$req->execute(array(':username' => $username));
+	$req = $db->prepare($sql);
+	$req->execute(array(':email' => $email));
 
-		$result = $req->fetch(PDO::FETCH_ASSOC);
+	$result = $req->fetch(PDO::FETCH_ASSOC);
 
-		if(!empty($result)) return false;
-		else return true;
-	}
+	if (!empty($result)) return false;
+	else return true;
+}
 
-	/*2.0!isMusicOwner*/
-	function isMusicOwner(PDO $db, $music_id, $user_id){
-		$sql = "
+/*1.9!isUsernameAvailable*/
+function isUsernameAvailable(PDO $db, $username)
+{
+	$sql = "SELECT id FROM users WHERE username = :username LIMIT 1";
+
+	$req = $db->prepare($sql);
+	$req->execute(array(':username' => $username));
+
+	$result = $req->fetch(PDO::FETCH_ASSOC);
+
+	if (!empty($result)) return false;
+	else return true;
+}
+
+/*2.0!isMusicOwner*/
+function isMusicOwner(PDO $db, $music_id, $user_id)
+{
+	$sql = "
 			SELECT
 				COUNT(id) AS is_owner
 			FROM
@@ -251,25 +260,25 @@ SUMMARY
 				user_id = :user_id
 		";
 
-		$req = $db->prepare($sql);
-		$req->execute(array(
-			':music_id' => $music_id,
-			':user_id' => $user_id
-		));
+	$req = $db->prepare($sql);
+	$req->execute(array(
+		':music_id' => $music_id,
+		':user_id' => $user_id
+	));
 
-		$result = $req->fetch();
+	$result = $req->fetch();
 
-		if($result['is_owner'] > 0){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}	
+	if ($result['is_owner'] > 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
-	/*2.1!updateProfilPicture*/
-	function updateProfilPicture(PDO $db, $pic, $user_id){
-		$sql = "
+/*2.1!updateProfilPicture*/
+function updateProfilPicture(PDO $db, $pic, $user_id)
+{
+	$sql = "
 								UPDATE
 									users
 								SET
@@ -278,9 +287,9 @@ SUMMARY
 									 id = :id
 							";
 
-		$req = $db->prepare($sql);
-		$req->execute(array(
-			':pic' => $pic,
-			':id' => $user_id
-		));
-	}
+	$req = $db->prepare($sql);
+	$req->execute(array(
+		':pic' => $pic,
+		':id' => $user_id
+	));
+}
