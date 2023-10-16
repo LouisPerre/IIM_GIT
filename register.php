5a440b853b7afb8013501bf4d4c536f8978dcff9
Adel Khiter <khiter.adel@gmail.com>
2023-10-16 12:46:22 +0200

    feat(register): Create function to register

--- register.php
+++ register.php
@@ -3,8 +3,24 @@ require('config/config.php');
 require('model/functions.fn.php');
 
 /*===============================
-	Register
+    Register
 ===============================*/
+
+if (isset($_POST['emailForm']) && isset($_POST['passwordForm']) && isset($_POST['usernameForm'])) {
+	if (!empty($_POST['emailForm']) && !empty($_POST['passwordForm']) && !empty($_POST['usernameForm'])) {
+
+		// TODO
+		$emailAvailability = isEmailAvailable($db, $_POST['emailForm']);
+		$usernameAvailability = isUsernameAvailable($db, $_POST['usernameForm']);
+		if ($usernameAvailability !== true || $emailAvailability !== true) {
+			$error = 'Either the email or username is not available';
+		}
+		// Force user connection to access dashboard
+		userRegistration($db, $_POST['usernameForm'], $_POST['emailForm'], $_POST['passwordForm']);
+	} else {
+		$error = 'Champs requis !';
+	}
+}
 
 include 'view/_header.php';
 include 'view/register.php';
